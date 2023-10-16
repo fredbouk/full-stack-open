cd041f60a18816769c8c6e07112154f5abbb8117
fredbouk <fredbouk@gmail.com>
2023-10-16 15:30:55 +0200

    add exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,17 +14,28 @@ const Statistic = (props) => (
   <p>{props.text}: {props.stat}</p>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = () => good + neutral + bad
+  const average = () => (good - bad) / total()
+  const positive = () => `${(good / total() * 100).toFixed(1)} %`
+  return (
+    <>
+      <Statistic text='Good' stat={good} />
+      <Statistic text='Neutral' stat={neutral} />
+      <Statistic text='Bad' stat={bad} />
+
+      <Statistic text='All' stat={total()} />
+      <Statistic text='Average' stat={average()} />
+      <Statistic text='Positive' stat={positive()} />
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const total = () => good + neutral + bad
-
-  const average = () => (good - bad) / total()
-
-  const positive = () => `${(good / total() * 100).toFixed(1)} %`
 
   return (
     <>
@@ -36,13 +47,7 @@ const App = () => {
 
       <Header text='Statistics' />
 
-      <Statistic text='Good' stat={good} />
-      <Statistic text='Neutral' stat={neutral} />
-      <Statistic text='Bad' stat={bad} />
-
-      <Statistic text='All' stat={total()} />
-      <Statistic text='Average' stat={average()} />
-      <Statistic text='Positive' stat={positive()} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
